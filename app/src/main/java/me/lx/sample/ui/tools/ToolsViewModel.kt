247927f71cafe3b3a2e09108345238eb3609570f
xiong.94@163.com <xiong.94@163.com>
2019-08-28 11:47:50 +0800

    新建项目

---
+++ app/src/main/java/me/lx/sample/ui/tools/ToolsViewModel.kt
@@ -0,0 +1,13 @@
+package me.lx.sample.ui.tools
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ToolsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is tools Fragment"
+    }
+    val text: LiveData<String> = _text
+}
